1143a89444932a845de5b6249c1ce485d331e410
ctupper1 <ctupper1@spscc.edu>
2023-06-21 19:13:35 -0700

    A boat load of changes and added unit tests

---
+++ PlantSalesApp/PlantSalesData/Session.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantSalesData
+{
+    public static class Session
+    {
+        public static int UserId { get; set; }
+        public static bool IsAdmin { get; set; }
+    }
+}
